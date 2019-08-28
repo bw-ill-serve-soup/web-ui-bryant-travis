3663136ca9ae4d3874086c0801e07df281f3e70f
tlaudahl <travis.laudahl@gmail.com>
2019-08-28 15:04:49 -0500

    Almost finished the About page, have hover effects done and layout done. Need to work on mobile sizing

---
+++ javascript/icons.js
@@ -0,0 +1,36 @@
+let githubIcon = document.querySelectorAll('.fa-github'); // 8 elements
+let linkedInIcon = document.querySelectorAll('.fa-linkedin-in'); // 7 elements
+
+// Daisy
+githubIcon[0].addEventListener('click', e => window.open('https://github.com/daisymesa', '_blank'));
+linkedInIcon[0].addEventListener('click', e => window.open('https://www.linkedin.com/in/mdaisy/', '_blank'));
+
+// Travis
+githubIcon[1].addEventListener('click', e => window.open('https://github.com/tlaudahl', '_blank'));
+linkedInIcon[1].addEventListener('click', e => window.open('https://www.linkedin.com/in/travis-laudahl', '_blank'));
+
+// Bryant
+githubIcon[2].addEventListener('click', e => window.open('https://github.com/BryantErwin', '_blank'));
+
+// Pere
+// githubIcon[3].addEventListener('click', e => window.open('https://github.com/', '_blank'));
+// linkedInIcon[3].addEventListener('click', e => window.open('www.linkedin.com/in/', '_blank'));
+
+// Zach
+githubIcon[4].addEventListener('click', e => window.open('https://github.com/zpallday', '_blank'));
+
+// Ryan
+githubIcon[5].addEventListener('click', e => window.open('https://github.com/74ultra', '_blank'));
+linkedInIcon[4].addEventListener('click', e => window.open('https://www.linkedin.com/in/ryanbarnhill/', '_blank'));
+
+// Mason
+githubIcon[6].addEventListener('click', e => window.open('https://github.com/karsevar', '_blank'));
+linkedInIcon[5].addEventListener('click', e => window.open('https://www.linkedin.com/in/masonkarsevar', '_blank'));
+
+// Jarvise
+githubIcon[7].addEventListener('click', e => window.open('https://github.com/Fullmetal235', '_blank'));
+linkedInIcon[6].addEventListener('click', e => window.open('https://www.linkedin.com/in/jarvise-billups-brown-a41a44172/', '_blank'));
+
+// Greg
+githubIcon[8].addEventListener('click', e => window.open('https://github.com/Zolton', '_blank'));
+linkedInIcon[7].addEventListener('click', e => window.open('https://linkedin.com/in/greg-zolton/', '_blank'));
